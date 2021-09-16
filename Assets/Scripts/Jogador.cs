5d8db5b04b17fc39b043d12a2dc67f228901f365
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-16 10:16:53 -0300

    Movimentação do jogador implementada

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -6,6 +6,8 @@ public class Jogador : MonoBehaviour
 {
     public Rigidbody rb;
 
+    public float velocidade = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +17,9 @@ public class Jogador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var v = Input.GetAxis("Vertical");
+        var v = Input.GetAxis("Vertical") * velocidade;
+        var h = Input.GetAxis("Horizontal") * velocidade;
 
-        print(v);
+        rb.velocity = new Vector3(h, rb.velocity.y, v);
     }
 }
